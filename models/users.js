be40952f11048037408394a658989c30a2a1217f
PTMcEneaney <pagemceneaney@gmail.com>
2019-07-23 10:52:11 -0700

    switched from moddals to login page, updated users model to include validation, updated styling on 404 and maps pages'

--- models/users.js
+++ models/users.js
@@ -1,14 +1,43 @@
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
+var bcrypt = require("bcryptjs");
+
 module.exports = function(sequelize, DataTypes) {
-  var Users = sequelize.define(
-    "Users",
-    {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING
+  // var Users = sequelize.define(
+  //   "Users",
+  //   {
+  //     email: DataTypes.STRING,
+  //     password: DataTypes.STRING
+  //   },
+  //   {
+  //     timestamps: false
+  //   }
+  // );
+  var Users = sequelize.define("Users", {
+    // The email cannot be null, and must be a proper email before creation
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
     },
-    {
-      timestamps: false
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
-  );
+  });
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  Users.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  Users.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
+
   Users.associate = function(models) {
     Users.belongsToMany(models.Songs, {
       through: "SwingTable",
